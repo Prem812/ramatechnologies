3d4da96f168106ffe608b6ae914229fa0ed004c8
Prem Sagar Shah <prem.shah8120@gmail.com>
2024-04-30 16:15:53 +0530

    added about page but need to have a look at least one time

--- ramatechnologies.in/routes/web.php
+++ ramatechnologies.in/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\Frontend\AboutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,3 +15,4 @@ use App\Http\Controllers\Frontend\HomeController;
 |
 */
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'index']);
